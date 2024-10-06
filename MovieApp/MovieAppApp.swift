33f7b1a7b711cb33de2f6a80db69e2e6b033407a
Milton <miltonputallaz@gmail.com>
2024-10-06 20:19:11 -0300

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Milton Putallaz on 06/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
